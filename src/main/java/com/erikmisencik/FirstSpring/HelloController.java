d05ca11bb3c51180fcbd1ead334120f0428a45ca
Erik Misencik <erik.misencik@student.tuke.sk>
2025-04-26 14:14:02 +0200

    Using PathVariable

--- src/main/java/com/erikmisencik/FirstSpring/HelloController.java
+++ src/main/java/com/erikmisencik/FirstSpring/HelloController.java
@@ -1,12 +1,14 @@
 package com.erikmisencik.FirstSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse sayHelloParam(@PathVariable String name) {
+        return new HelloResponse("Hello, "+ name + "!");
+    }
 
     @GetMapping("/hello")
     public HelloResponse sayHello() {
